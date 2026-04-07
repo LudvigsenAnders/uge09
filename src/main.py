8bda70f7690c5062b4c9804cb052ef809490dd1b
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-04-07 14:58:38 +0200

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -42,21 +42,69 @@ print(df.info())
 
 ##### Feature engineering
 df = df.sort_values(by=["Ticker", "Date"])
-df['trend'] = 0
-df.loc[df.groupby('Ticker')['Close'].diff() > 0, 'trend'] = 1
-df.loc[df.groupby('Ticker')['Close'].diff() <= 0, 'trend'] = 0
+df['target'] = 0
+df.loc[df.groupby('Ticker')['Close'].diff() > 0, 'target'] = 1
+df.loc[df.groupby('Ticker')['Close'].diff() <= 0, 'target'] = 0
+
+
+cat_cols = ["Ticker"]
+
+df[cat_cols] = df[cat_cols].astype("category")
+
+
+df["year"] = df["Date"].dt.year
+df["month"] = df["Date"].dt.month
+df["day_of_month"] = df["Date"].dt.day
+df = df.drop(columns=["Date"])
+
+print(df.info())
+
+
+X = df.drop(columns=["target"])
+y = df["target"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    stratify=y,
+    random_state=42
+)
+
+model = XGBClassifier(
+    objective="binary:logistic",
+    eval_metric="auc",
+    enable_categorical=True,
+    n_estimators=300,
+    max_depth=6,
+    learning_rate=0.05,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    random_state=42
+
+)
+
+model.fit(X_train, y_train)
+
+y_proba = model.predict_proba(X_test)[:, 1]
+y_pred = (y_proba >= 0.5).astype(int)
 
 
 
+print("ROC‑AUC:", roc_auc_score(y_test, y_proba))
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
 
-#df_ticker_1 = df[df["Ticker"] == "ticker_1"]
-df_ticker_y2020 = df[df["Date"].dt.year == 2020]
 
-fig1 = df_ticker_y2020.plot(x="Date", y="Close", kind="line", title="Closing Price Over Time")
+# #df_ticker_1 = df[df["Ticker"] == "ticker_1"]
+# df_ticker_y2020 = df[df["Date"].dt.year == 2020]
 
-print(df_ticker_y2020.head(10))
-print(df_ticker_y2020.tail(10))
+# fig1 = df_ticker_y2020.plot(x="Date", y="Close", kind="line", title="Closing Price Over Time")
 
-plt.show()
+# print(df_ticker_y2020.head(10))
+# print(df_ticker_y2020.tail(10))
+
+# plt.show()
 
 
